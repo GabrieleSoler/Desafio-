5fce63ec2f146fef9cb6c5544c36a6a1b11f64d8
GabrieleSoler <gabriele.dasolervieira@gmail.com>
2022-03-01 22:21:02 -0300

    Atualização
    Atualizado o imc.js

--- exercicio7/imc.js
+++ exercicio7/imc.js
@@ -6,8 +6,10 @@ function imc () {
     const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
 
-    if (nome !== '' && altura !== '' && peso !== '') {
+    //Aqui estou pegando os elementos pelo id e atribuindo nas varíaveis
 
+    if (nome !== '' && altura !== '' && peso !== '') {
+    // Se o nome, altura e peso não estiverem vazios então ele vai calcular o imc
         const valorIMC = (peso / (altura * altura)).toFixed(1);
 
         let classificacao = '';
@@ -19,15 +21,19 @@ function imc () {
         }else if (valorIMC > 25){
             classificacao = 'acima do peso';
         }else {
-            classificacao = 'com obesidade grau III. Cuidado!!';
+            classificacao = 'com obesidade grau III.';
+    //Aqui é feito a classificação do imc de acordo com o resultado que o usuário digitou.
+
         }
 
         resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
-        
+        //Aqui é mostrado na tela para o usuário o imc dele.
     }else {
         resultado.textContent = 'Preencha todos os campos';
+        //Esse else é para caso o usuário não digite nenhum campo
     }
 
 }
 
 calcular.addEventListener('click', imc);
+//Quando alguem clicar no botão, vai ser executado a função imc
